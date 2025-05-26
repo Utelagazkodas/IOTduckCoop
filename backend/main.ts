71cff7ac00a20a18de284e2db56b9dc09847d756
utelagazkodas <vargamiki53@gmail.com>
2025-05-26 22:01:42 +0200

    aa

--- backend/main.ts
+++ backend/main.ts
@@ -3,46 +3,17 @@ import { generate } from "@alikia/random-key";
 
 const PASSWORD = Deno.env.get("PASSWORD");
 const SESSIONTOKENLENGTH = Number(Deno.env.get("SESSIONTOKENLENGTH"))
-const SESSIONTOKENVALID = Number(Deno.env.get("SESSIONTOKENVALID"))
+const SESSIONTOKENVALIDLENGTH = Number(Deno.env.get("SESSIONTOKENVALIDLENGTH"))
+
+if(!PASSWORD || !SESSIONTOKENLENGTH || !SESSIONTOKENVALIDLENGTH){
+  throw "You need to initialize the project, you can do it by running `deno task gen`"
+}
 
 const SessionTokens: { [key: string]: number } = {};
 
 Deno.serve({ port: 5000 }, (req) => {
-  if (req.headers.get("upgrade") != "websocket") {
-    return new Response(null, { status: 501 });
-  }
-  const { socket, response } = Deno.upgradeWebSocket(req);
 
-  let loggedIn = false;
 
-  socket.addEventListener("message", (event) => {
-    try {
-      if(!loggedIn){
-        let loginData : {password : string, sessionToken : boolean | string} = JSON.parse(event.data)
-
-        if(loginData.password && loginData.sessionToken && typeof(loginData.sessionToken) == "boolean"){
-          if(loginData.password == PASSWORD){
-            loggedIn = true
-
-            if(loginData.sessionToken == true){
-
-            }
-          }
-          else{
-            
-          }
-        }else if(typeof(loginData.sessionToken) == "string"){
-          console.log("sessiontoken")
-        }else {
-          console.log("login data formated badly")
-        }
-      }
-      else{
-        socket.send("you are logged in")
-      }
-    } catch (error) {
-      console.log("Bad message, probably not JSON")
-    }
-  });
-  return response;
+  return new Response("asd")
+  
 });
--- backend/util.ts
+++ backend/util.ts
@@ -1,3 +1,9 @@
 export function getUnixTime(): number {
     return Math.round(Date.now() / 1000)
 }
+
+export function getInANumber(promptText : string, min? : number, max? : number, whole : boolean = true) :number{
+    let ret : number
+    
+    ret = Number(prompt(promptText))
+}
