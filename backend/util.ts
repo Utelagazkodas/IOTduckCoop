a0361aec7e34a8361f0d37913bb2792a37c3662b
utelagazkodas <vargamiki53@gmail.com>
2025-05-26 22:02:38 +0200

    ?

--- backend/util.ts
+++ backend/util.ts
@@ -6,4 +6,24 @@ export function getInANumber(promptText : string, min? : number, max? : number, 
     let ret : number
     
     ret = Number(prompt(promptText))
+
+    while(!ret || (min && ret < min) || (max && max < ret) || (ret%1 != 0  && whole)) {
+        if(!ret){
+            console.error("The input wasnt a number or there was a miscalenous error, try again")
+            ret = Number(prompt(promptText))
+            continue
+        }
+        if(min && ret < min){
+            console.error("The inputted number needs to be bigger than "+ min )
+            ret = Number(prompt(promptText))
+            continue
+        }
+        if(max && max < ret){
+            console.error("The inputted number needs to be bigger than "+ max )
+            ret = Number(prompt(promptText))
+            continue
+        }
+    }
+
+    return ret
 }
