0080ecb9b4464a52d5884ec59e02b7c6e3166cc0
utelagazkodas <vargamiki53@gmail.com>
2025-06-15 22:05:24 +0200

    start

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.css';
+	
+	let { children } = $props();
+
+	import { initApi } from "$lib/api"
+    import { onMount } from 'svelte';
+
+	onMount(initApi)
+</script>
+
+{@render children()}
