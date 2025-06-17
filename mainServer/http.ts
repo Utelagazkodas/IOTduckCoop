31d7c8bcfaf5fe365b9798e9744a9966ccd22e80
utelagazkodas <vargamiki53@gmail.com>
2025-06-17 15:56:02 +0200

    aaa

--- mainServer/http.ts
+++ mainServer/http.ts
@@ -1,6 +1,5 @@
 import { get } from "./http/get.ts";
 import { post } from "./http/post.ts";
-import { handleWebsocket } from "./ws/websocket.ts";
 
 export async function handleRequest(
   req: Request,
@@ -10,27 +9,6 @@ export async function handleRequest(
   let resp: Response = new Response();
 
   console.log(`Incoming request: ${req.method} ${req.url}`);
-
-  //checks if it is trying to connect to ws
-  if (url.pathname == "/connect" || req.headers.get("upgrade") == "websocket") {
-    if (url.pathname != "/connect") {
-      return new Response("you can only connect as a websocket on /connect", {
-        status: 403,
-      });
-    }
-    if (req.headers.get("upgrade") != "websocket") {
-      return new Response(
-        "on /connect you can only upgrade to a websocket connection, and you need to have upgrade: websocket header",
-        { status: 426 },
-      );
-    }
-
-    const { socket, response } = Deno.upgradeWebSocket(req);
-
-    handleWebsocket(socket)
-
-    return response;
-  }
 
   // switches between the supported methods
   switch (req.method) {
@@ -44,7 +22,10 @@ export async function handleRequest(
       return new Response("Method is not supported", { status: 405 });
   }
 
-  // adds access control allow origin to all responses because it is needed so it works even when its a different ip and shit
-  resp.headers.set("Access-Control-Allow-Origin", "*"); // Allows requests from any origin
+  // tries adding access control allow origin to all responses because it is needed so it works even when its a different ip and shit, it will ALWAYS fail when websocket is trying to connect
+  try {
+    resp.headers.set("Access-Control-Allow-Origin", "*"); // Allows requests from any origin
+  } catch (_e) { /**/ }
+  
   return resp;
 }
--- mainServer/http/get.ts
+++ mainServer/http/get.ts
@@ -1,7 +1,33 @@
+import { handleWebsocket } from "../ws/websocket.ts";
+
 export async function get(
   req: Request,
   inf: Deno.ServeHandlerInfo<Deno.NetAddr>,
   url: URL,
 ): Promise<Response> {
+
+  //checks if it is trying to connect to ws
+  if (url.pathname == "/connect" || req.headers.get("upgrade") == "websocket") {
+    if (url.pathname != "/connect") {
+      return new Response("you can only connect as a websocket on /connect", {
+        status: 403,
+      });
+    }
+    if (req.headers.get("upgrade") != "websocket") {
+      return new Response(
+        "on /connect you can only upgrade to a websocket connection, and you need to have upgrade: websocket header",
+        { status: 426 },
+      );
+    }
+
+    const { socket, response } = Deno.upgradeWebSocket(req);
+
+    handleWebsocket(socket)
+
+    return response;
+  }
+
+
+
   return new Response("Bad GET request", { status: 400 });
 }
