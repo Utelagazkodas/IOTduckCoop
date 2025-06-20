02364cda452687798bd7b385d1cc2a0e150e3c43
utelagazkodas <vargamiki53@gmail.com>
2025-06-20 12:46:59 +0200

    laptop dead

--- mainServer/main.ts
+++ mainServer/main.ts
@@ -1,7 +1,10 @@
 import { handleRequest } from "./http.ts";
 import { runtimeData } from "./utility/classes.ts";
+import { requireEnv } from "./utility/util.ts";
 
-export const runtimeData : runtimeData = JSON.parse(Deno.readTextFileSync("./database/data.json"))
+export const RUNTIMEDATA : runtimeData = JSON.parse(Deno.readTextFileSync("./database/data.json"))
+
+export const PORT = Number(requireEnv("PORT"))
 
 
 Deno.serve(handleRequest)
